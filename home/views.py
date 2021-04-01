d2754fea4b4350d08a6b2090fb27a656d2d10b8e
Arrowoon <arrowoonbijay@gmail.com>
2021-04-01 13:18:40 +0545

    product

--- home/views.py
+++ home/views.py
@@ -19,7 +19,28 @@ class HomeView(BaseView):
 class ItemDetailView(BaseView):
     def get(self,request,slug):
         self.views['item_detail'] = Item.objects.filter(slug=slug)
+        self.views['brand'] = Brand.objects.filter(status= 'active')
+        self.views['count'] = []
+        for i in self.views['brand']:
+            count_food = Item.objects.filter(brand=i.id).count()
+            print(i.name,count_food)
+            d = {'name':i.name,'count':count_food}
+            self.views['count'].append(d)
+            print(d)
+
+        self.views['count_cat'] = Category.objects.filter(status= 'active')
+        self.views['cat_count'] = []
+        for i in self.views['count_cat']:
+            count_food = Item.objects.filter(category=i.id).count()
+            print(i.name,count_food)
+            d = {'name':i.name,'image':i.image,'cat_count':count_food}
+            self.views['cat_count'].append(d)
+            print(d)
+
         cat = Item.objects.get(slug = slug).category_id
         self.views['catitems'] = Item.objects.filter(category=cat)
         return render(request,'product-detail.html',self.views)
+
+
+
 
